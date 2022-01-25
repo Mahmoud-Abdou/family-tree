015f5d42be8ad20053c834a094da6238ed1b786d
mahmoud-abdou <mahmoudabdou.eg@gmail.com>
2022-01-25 18:00:55 +0200

    news, deaths and events edits

---
+++ resources/views/web_app/News/index.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.master')
+
+@section('page-title', $pageTitle)
+
+@section('breadcrumb')
+    @include('partials.breadcrumb', ['pageTitle' => '<i class="ri-home-line"> </i>'.$menuTitle, 'slots' => [['title' => $menuTitle, 'link' => route('home')],]])
+@endsection
+
+@section('content')
+    <div id="content-page" class="content-page">
+        <div class="container-fluid">
+            <div class="row">
+
+                <div class="col-lg-12">
+                    @include('partials.messages')
+
+                    <div class="card iq-mb-3">
+                        <div class="card-header">
+                            <h5 class="float-left my-auto"><i class="ri-map-2-line"> </i> {{ $menuTitle }}</h5>
+                            @can('news.create')
+                                <a href="{{ route('news.create') }}" class="btn btn-primary rounded-pill float-right"><i class="ri-add-fill"> </i>اضافة</a>
+                            @endcan
+                        </div>
+                        <div class="card-body p-0">
+
+                            <div class="table-responsive">
+                                <table class="table m-0 px-2">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">المدينة</th>
+                                        <th scope="col">عنوان</th>
+                                        <th scope="col">وصف </th>
+                                        <th scope="col">المالك </th>
+                                        <th scope="col">الإجراءات</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @if($news->count() > 0)
+                                        @foreach($news as $row_news)
+                                            <tr>
+                                                <td>{{ $row_news->city->name_ar }}</td>
+                                                <td>{{ $row_news->title }}</td>
+                                                <td>{{ $row_news->body }}</td>
+                                                <td>{{ $row_news->owner->name }}</td>
+                                                <td>
+                                                    <div class="d-flex justify-center">
+                                                        @if($row_news->owner_id == auth()->user()->id)
+                                                            @can('news.update')
+                                                            <a class="btn btn-outline-warning rounded-pill mx-1" href="{{ route('news.edit', $row_news) }}"><i class="ri-edit-2-fill"></i></a>
+                                                            @endcan
+                                                            @can('news.delete')
+                                                            <form action="{{ route('news.destroy', $row_news) }}" method="POST">
+                                                                @csrf
+                                                                @method('DELETE')
+
+                                                                <button type="submit" class="btn btn-outline-danger rounded-pill mx-1"><i class="ri-delete-back-2-fill"></i></button>
+                                                            </form>
+                                                            @endcan
+                                                        @endif
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    @else
+                                        <tr>
+                                            <td colspan="7" class="text-center"> لا توجد بيانات </td>
+                                        </tr>
+                                    @endif
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            <div class="d-flex justify-content-around">{{ $news->links() }}</div>
+                        </div>
+
+                        <div class="card-footer text-muted">
+                            مجموع عدد السجلات
+                            <span class="badge badge-pill border border-dark text-dark">{{ $news->count() }}</span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/web_app/News/update.blade.php
@@ -0,0 +1,85 @@
+@extends('layouts.master')
+
+@section('page-title', $pageTitle)
+
+@section('breadcrumb')
+    @include('partials.breadcrumb', ['pageTitle' => '<i class="ri-map-2-line"> </i>'.$menuTitle, 'slots' => [['title' => 'الدول و المدن', 'link' => route('news.index')],['title' => $menuTitle, 'link' => route('roles.create')],]])
+@endsection
+
+@section('content')
+    <div id="content-page" class="content-page">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+
+                    @include('partials.messages')
+                    @include('partials.errors-messages')
+
+                    <div class="card iq-mb-3">
+                        <div class="card-header">
+                            <h5 class="float-left my-auto"><i class="ri-map-2-line"> </i> {{ $menuTitle }}</h5>
+                        </div>
+                        <div class="card-body">
+                            <form dir="rtl" method="POST" action="{{ route('news.update', $news) }}" enctype="multipart/form-data" >
+                                @csrf
+                                @method('PUT')
+                                <div class="row">
+
+                                    <div class="form-group col-lg-6">
+                                        <label for="city_id">المدينة</label>
+                                        <select name="city_id" id="city_id" class="form-control mb-0" required autofocus>
+                                            <option disabled>اختر المدينة</option>
+                                            @foreach($cities as $city)
+                                                @if($city->id == $news->city->id)
+                                                    <option value="{{$city->id}}" selected>{{ $city->name_ar }}</option>
+                                                @else
+                                                    <option value="{{$city->id}}">{{ $city->name_ar }}</option>
+                                                @endif
+                                            @endforeach
+                                        </select>    
+                                    </div>
+                                    <div class="form-group col-lg-6">
+                                        <label for="title">العنوان</label>
+                                        <input type="text" name="title" class="form-control mb-0" id="title" tabindex="2" value="{{ $news->title }}" required autofocus>
+                                    </div>
+                                    <div class="form-group col-lg-6">
+                                        <label for="body">الوصف</label>
+                                        <input type="text" name="body" class="form-control mb-0" id="body" tabindex="3" value="{{ $news->body }}" required autofocus>
+                                    </div>
+                                   
+
+                                    <div class="form-group col-lg-6">
+                                        <label for="category_id">النوع</label>
+                                        <select name="category_id" id="category_id" class="form-control mb-0" required autofocus>
+                                            <option>اختر النوع</option>
+                                            @foreach($categories as $category)
+                                                @if($category->id == $news->category->id)
+                                                    <option value="{{$category->id}}" selected>{{ $category->name_ar }}</option>
+                                                @else
+                                                    <option value="{{$category->id}}">{{ $category->name_ar }}</option>
+                                                @endif
+                                            @endforeach
+                                        </select> 
+                                    </div>
+                                    
+
+                                    <div class="form-group col-lg-6">
+                                        <div class="row flex inline-flex p-2 mx-2">
+                                            <button type="submit" class="btn px-5 btn-primary rounded-pill " tabindex="6"><i class="ri-save-2-fill"> </i>حفظ </button>
+                                        </div>
+                                    </div>
+                                    </div>
+
+                                
+
+                            </form>
+                        </div>
+
+                        <div class="card-footer text-muted"></div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
